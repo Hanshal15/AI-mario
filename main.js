8905799e3bfae5e03ad0b08d0af5526a1ed36637
Hanshal15 <77623196+Hanshal15@users.noreply.github.com>
2021-08-27 17:45:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
 function preload() {
 	world_start = loadSound("world_start.wav");
-	setSprites();
-	MarioAnimation();
+	setSprites(); //used to load all imgs
+	MarioAnimation(); //used to load all animations
 }
 
 function setup() {
 	canvas = createCanvas(1240,336);
 	canvas.parent("canvas");
 	instializeInSetup(mario);
+    video = createCapture(VIDEO);
+	video.size(600,300);
+	video.parent("game_console"); //parent function is used to put webcam or any component inside an HTML div
+	poseNet = ml5.poseNet(video,modelLoaded);
+	poseNet.on("pose",gotposes);
 }
 
 function draw() {
-	game()
+	game() //its used to start the game_console
 }
 
+function gotposes(results) {
+  if(results.length) {
+	  console.log(results);
+	  noseX = results[0].pose.nose.x;
+	  noseY = results[0].pose.nose.y;
+  }
+}
 
+function modelLoaded() {
+	console.log("model loaded");
+}
 
 
 
